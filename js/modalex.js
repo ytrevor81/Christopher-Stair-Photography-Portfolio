fecc7bb9abc53bb6968d2cafdfb5fa06e3b7e98c
Trevor Youngquist <46886041+ytrevor81@users.noreply.github.com>
2019-12-18 00:10:49 +0700

    about page redo

--- js/modalex.js
+++ js/modalex.js
@@ -1,3 +1,32 @@
+//Nav
+
+$(document).ready(function(){
+
+  $(".navbar").hide(); //Hide the navigation bar first
+
+    $(window).scroll(function () {  //Listen for the window's scroll event
+        if (isScrolledAfterElement("#About")) { //if it has scrolled beyond the #content elment
+            $('.navbar').fadeIn();  //Show the navigation bar
+        } else {
+            $('.navbar').fadeOut(); //Else hide it
+        }
+    });
+
+    //Function that returns true if the window has scrolled beyond the given element
+    function isScrolledAfterElement(elem) {
+        var $elem = $(elem);
+        var $window = $(window);
+
+        var docViewTop = $window.scrollTop();
+        var docViewBottom = docViewTop + $window.height();
+
+        var elemTop = $elem.offset().top;
+
+        return elemTop <= docViewBottom;
+    }
+});
+
+
 $( ".img-wrapper" ).hover(
   function() {
     $(this).find(".img-overlay").animate({opacity: 1}, 600);
