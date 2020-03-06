bd72706b6a34753ded0485835342dde5d8be99d4
Trevor Youngquist <46886041+ytrevor81@users.noreply.github.com>
2020-03-06 17:29:19 +0700

    ready to deploy to heroku

--- js/email.js
+++ js/email.js
@@ -99,3 +99,12 @@
     }
   }
 })();
+
+//Email Notification
+
+$('#contact-form').submit(function(e) {
+  $('#messages').removeClass('hide').addClass('alert alert-success alert-dismissible').slideDown().show();
+  $('#messages_content').html('<h4>Email Sent!</h4>');
+  $('#modal').modal('show');
+    e.preventDefault();
+  });
